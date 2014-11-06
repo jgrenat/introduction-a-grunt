0d6182d19a75eee5bd4a11c484079343b517f9fa
Jordan Grenat <jordan.grenat@gmail.com>
2014-11-07 00:05:33 +0100

    Change variable name

--- tasks/build.js
+++ tasks/build.js
@@ -1,14 +1,14 @@
 module.exports = function(grunt) {
-    grunt.registerTask('build', function(target) {
-	target = target || 'prod';
-	grunt.task.run([
-	    'clean:dist',
-	    'copy:dist',
-	    'useminPrepare',
-	    'cssmin', 
-	    'uglify',
-	    'usemin',
-	    'replace-config-vars:' + target
-	]);
+    grunt.registerTask('build', function(environment) {
+		environment = environment || 'preprod';
+		grunt.task.run([
+		    'clean:dist',
+		    'copy:dist',
+		    'useminPrepare',
+		    'cssmin', 
+		    'uglify',
+		    'usemin',
+		    'replace-config-vars:' + environment
+		]);
     });
 };
